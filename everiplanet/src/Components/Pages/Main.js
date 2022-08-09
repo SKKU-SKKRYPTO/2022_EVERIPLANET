a729dd87aad952e3afd12939bb527b94462d0a6f
chomyungha <62551858+chomyungha51@users.noreply.github.com>
2022-08-09 23:28:18 +0900

    Yusung's work

--- everiplanet/src/Components/Pages/Main.js
+++ everiplanet/src/Components/Pages/Main.js
@@ -1,17 +1,21 @@
 import React from "react"
+import messages from '../../common/messages';
 
-function Main() {
-
-    return (
+const MainPage = () => {
+  return (
+    <div>
+        {messages.welcome}
         <div>
-            This is Main<br></br>
-            <a href="http://localhost:3000/factory">Factory</a><br></br>
-            <a href="http://localhost:3000/Barrack">Barrack</a><br></br>
-            <a href="http://localhost:3000/DnaFactory">DnaFactory</a><br></br>
-            <a href="http://localhost:3000/Mine">Mine</a><br></br>
-            <a href="http://localhost:3000/Testpage">Testpage</a><br></br>
-        </div>
-    )
-}
+             This is Main<br></br>
+             <a href="http://localhost:3000/factory">Factory</a><br></br>
+             <a href="http://localhost:3000/Barrack">Barrack</a><br></br>
+             <a href="http://localhost:3000/DnaFactory">DnaFactory</a><br></br>
+             <a href="http://localhost:3000/Mine">Mine</a><br></br>
+             <a href="http://localhost:3000/Testpage">Testpage</a><br></br>
+         </div>
+   </div>
+  )
+ 
+};
 
-export default Main;
+export default MainPage;
