7ada8113909321ef17f5e951f404c6a3c4454439
sircoon4 <47733258+sircoon4@users.noreply.github.com>
2022-08-13 15:25:03 +0900

    add web3

--- everiplanet/src/Components/Pages/Testpage.js
+++ everiplanet/src/Components/Pages/Testpage.js
@@ -1,10 +1,34 @@
 import React from "react"
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { CONTACT_ABI, CONTACT_ADDRESS } from '../../configs/contract';
 
 function Testpage() {
+    const [account, setAccount] = useState();
+    const [brainies, setBrainies] = useState();
+    const [nftName, setNftName] = useState();
+
+    useEffect(() => {
+        async function load() {
+          const web3 = new Web3(Web3.givenProvider);
+          const account = await web3.eth.requestAccounts();
+    
+          setAccount(account)
+    
+          const _brainies = await new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS)
+          setBrainies(_brainies)
+    
+          const _nftName = await brainies.methods.name().call()
+          setNftName(_nftName)
+        }
+    
+        load();
+    }, []);
 
     return (
         <div>
-            This is Testpage
+            <div> Your account is: {account} </div>
+            <div> Brainies nftName is: {nftName} </div>
         </div>
     )
 }
